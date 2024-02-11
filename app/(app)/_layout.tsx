ee9663923a6b52f981c8e056fec5483b07e515bf
ArunKumarBhardwaj <bhardwajarun38@gmail.com>
2024-02-11 12:07:46 +0530

    typo fix

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Stack } from "expo-router";
 
-export default function TabLayout() {
+export default function AppLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
